9c082f769c20f429fe2e8d1ca344477d43d27d74
Joan Madrid <jmadrid@omitsis.com>
2016-07-20 10:18:13 +0200

    Fix upcode.sh delete assetic:dump

--- upcode.sh
+++ upcode.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-chmod 777 -R app/cache
-chmod 777 -R app/logs
+sudo chmod 777 -R app/cache
+sudo chmod 777 -R app/logs
 composer install
 php app/console cache:clear --no-warmup
 php app/console cache:clear --env=prod --no-warmup
-#echo "y" | php app/console doc:mig:mig
 php app/console doc:sc:up --force
-php app/console assetic:dump
 php app/console assets:install web
-chmod 777 -R app/cache
-chmod 777 -R app/logs
+sudo chmod 777 -R app/cache
+sudo chmod 777 -R app/logs
